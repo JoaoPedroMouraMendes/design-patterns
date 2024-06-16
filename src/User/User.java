12689b1bf95fdaa4ecd6a1179cd8eb42f2d74e92
João Pedro <joaopedromouramendes1@gmail.com>
2024-06-16 17:22:09 -0300

    create: User with UserBuilder

---
+++ src/User/User.java
@@ -0,0 +1,73 @@
+package User;
+
+public class User {
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+
+    private User(String name, String lastName, String email, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public static class UserBuilder {
+        private String name;
+        private String lastName;
+        private String email;
+        private String password;
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User build() {
+            return new User(name, lastName, email, password);
+        }
+    }
+
+}
